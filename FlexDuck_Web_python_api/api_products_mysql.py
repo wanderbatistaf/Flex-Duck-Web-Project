ef7a6c1fcc608bf894bfc4839a51442b4639a857
Nelson Fernando Santos Eltz <nelson.eltz@picpay.com>
2023-06-01 10:45:17 -0300

    +products_api

--- FlexDuck_Web_python_api/api_products_mysql.py
+++ FlexDuck_Web_python_api/api_products_mysql.py
@@ -9,7 +9,7 @@ api_products = Blueprint('api_products', __name__)
 # Configura a conexão com o banco de dados MySQL
 db = mysql_connector.db
 
-# API SUPPLIERS #
+# API PRODUCTS #
 # Define a rota GET para buscar dados do banco de dados
 @api_products.route('/products', methods=['GET'])
 @jwt_required() # Protege a rota com JWT
@@ -47,3 +47,69 @@ def buscar_dados():
         "items": items
     }
     return jsonify(response)
+
+# Define a rota GET para buscar dados do banco de dados especifico
+@api_suppliers.route('/products/<int:id>', methods=['GET'])
+@jwt_required() # Protege a rota com JWT
+def buscar_dados_user(id):
+    current_user = get_jwt_identity()
+    if not current_user:
+        return abort(404)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM produtos_servicos WHERE id = %s'
+    val = (id,)
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    cursor.close()
+    if result:
+        return jsonify({'mensagem': 'Produto localizado com sucesso!', 'produto': result})
+    else:
+        return jsonify({'mensagem': 'Produto não encontrado!'}), 404
+    
+# Define a rota POST para inserir dados no banco de dados
+@api_suppliers.route('/products/add', methods=['POST'])
+@jwt_required()
+def inserir_dados():
+    current_user = get_jwt_identity()
+    if not current_user:
+        return abort(404)
+    dados = request.json
+    cursor = db.cursor()
+    sql = 'INSERT INTO produtos_servicos (codigo, descricao, nome, categoria,marca,preco_venda, preco_custo, margem_lucro,desconto,quantidade, localizacao, estoque_minimo, estoque_maximo, alerta_reposicao, fornecedor_id,client_id    ) VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s)'
+    val = (dados['codigo'], dados['descricao'], dados['nome'], dados['categoria'],dados['marca'],dados['preco_venda'], dados['preco_custo'], dados['margem_lucro'],dados['desconto'],dados['quantidade'], dados['localizacao'], dados['estoque_minimo'], dados['estoque_maximo'], dados['alerta_reposicao'], dados['fornecedor_id'],dados['client_id'])
+    cursor.execute(sql, val)
+    db.commit()
+    print(cursor)
+    cursor.close()
+    return jsonify({'mensagem': 'Dados inseridos com sucesso!'})
+
+# Define a rota PUT para atualizar dados no banco de dados
+@api_suppliers.route('/products/update/<int:id>', methods=['PUT'])
+@jwt_required()
+def atualizar_dados(id):
+    current_user = get_jwt_identity()
+    if not current_user:
+        return abort(404)
+    dados = request.json
+    cursor = db.cursor()
+    sql = 'UPDATE produtos_servicos SET codigo= %s, descricao= %s, nome= %s, categoria= %s,marca= %s,preco_venda= %s, preco_custo= %s, margem_lucro= %s,desconto,quantidade= %s, localizacao= %s, estoque_minimo= %s, estoque_maximo= %s, alerta_reposicao= %s, fornecedor_id= %s,client_id= %s'
+    val = (dados['codigo'], dados['descricao'], dados['nome'], dados['categoria'],dados['marca'],dados['preco_venda'], dados['preco_custo'], dados['margem_lucro'],dados['desconto'],dados['quantidade'], dados['localizacao'], dados['estoque_minimo'], dados['estoque_maximo'], dados['alerta_reposicao'], dados['fornecedor_id'],dados['client_id'])
+    cursor.execute(sql, val)
+    db.commit()
+    cursor.close()
+    return jsonify({'mensagem': 'Dados atualizados com sucesso!'})
+
+# Define a rota DELETE para excluir dados do banco de dados
+@api_suppliers.route('/products/delete/<int:id>', methods=['DELETE'])
+@jwt_required() # Protege a rota com JWT
+def excluir_dados(id):
+    current_user = get_jwt_identity()
+    if not current_user:
+        return abort(404)
+    cursor = db.cursor()
+    sql = 'DELETE FROM produtos_servicos WHERE codigo = %s'
+    val = (codigo,)
+    cursor.execute(sql, val)
+    db.commit()
+    cursor.close()
+    return jsonify({'mensagem': 'Dados excluídos com sucesso!'})
