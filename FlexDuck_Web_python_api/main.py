7e3d1a3460bb942bf8981f03272b3b15d666dadd
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-06-12 02:34:45 -0300

    Update main.py
    +DbDown-FIX

--- FlexDuck_Web_python_api/main.py
+++ FlexDuck_Web_python_api/main.py
@@ -1,15 +1,27 @@
-from flask import Flask, jsonify, request, abort
+import sys
+import subprocess
+import time
+import threading
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from Controller import mysql_connector
 
 # Inicializa o aplicativo Flask
-# app = Flask(__name__)
-# CORS(app)  # Permite que o Flask responda a solicitações de qualquer origem
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['JWT_SECRET_KEY'] = '4Ndr3w5077' # Chave secreta do JWT
 jwt = JWTManager(app)
+
+# Função para reconectar ao banco de dados MySQL
+def reconnect_db():
+    try:
+        db.ping(reconnect=True)
+        print("Conexão estavel com o banco de dados")
+    except mysql_connector.errors.OperationalError:
+        print("Erro de conexão com o banco de dados. Tentando reconectar...")
+        db.reconnect()
+        print("Reconexão bem-sucedida ao banco de dados")
 
 # Configura a conexão com o banco de dados MySQL
 db = mysql_connector.db
@@ -39,13 +51,22 @@ def autenticar_usuario():
     user = cursor.fetchone()
     cursor.close()
     if user:
-        token_data = {'username': user['username'], 'level': user['level'], 'user_id': user['user_id'],
-                      'name': user['name'], 'active': user['active'], 'user_id': user['user_id']}
+        token_data = {
+            'username': user['username'],
+            'level': user['level'],
+            'user_id': user['user_id'],
+            'name': user['name'],
+            'active': user['active'],
+            'user_id': user['user_id']
+        }
         token = create_access_token(identity=token_data)
         print(token)
         return jsonify({'access_token': token})
     else:
-        return jsonify({'mensagem': 'Usuário ou senha inválidos'}), 401
+        # Encerra o aplicativo Flask
+        encerrar_aplicativo()
+        # Reinicia o aplicativo em um novo processo
+        reiniciar_aplicativo()
 
 @app.route('/auth/user-level', methods=['GET'])
 @jwt_required()
@@ -57,9 +78,36 @@ def get_user_level():
     else:
         return jsonify({'mensagem': 'Nível de acesso não encontrado'}), 404
 
+# Função para verificar a conexão a cada 5 minutos
+def check_db_connection():
+    while True:
+        try:
+            reconnect_db()
+        except Exception as e:
+            print("Erro ao verificar a conexão com o banco de dados:", str(e))
+            # Encerra o aplicativo Flask
+            encerrar_aplicativo()
+            # Reinicia o aplicativo em um novo processo
+            reiniciar_aplicativo()
+        time.sleep(50)  # Aguarda 2 minutos
 
+# Função para encerrar o aplicativo Flask
+def encerrar_aplicativo():
+    print("Encerrando o aplicativo...")
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is not None:
+        func()
 
+# Função para reiniciar o aplicativo em um novo processo
+def reiniciar_aplicativo():
+    print("Reiniciando o aplicativo...")
+    subprocess.Popen([sys.executable] + sys.argv)
+    sys.exit()
+
+# Thread para executar a função de verificação de conexão
+check_db_thread = threading.Thread(target=check_db_connection)
+check_db_thread.start()
 
 # Executa o aplicativo Flask
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
