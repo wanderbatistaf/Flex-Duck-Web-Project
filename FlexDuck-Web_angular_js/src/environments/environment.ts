79a2bf0f0afcaac13858f48453b6561e6b918b15
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-06-26 18:51:51 -0300

    +EnvUpdate

--- FlexDuck-Web_angular_js/src/environments/environment.ts
+++ FlexDuck-Web_angular_js/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://pz5sjz-5000.csb.app',
+    apiUrl: 'https://ykthsd-5000.csb.app',
     // apiUrl: 'http://127.0.0.1:5000',
     webUrl: 'https://flex-duck-web-project.vercel.app',
     // WebTestUrl: 'http://127.0.0.1:4200'
