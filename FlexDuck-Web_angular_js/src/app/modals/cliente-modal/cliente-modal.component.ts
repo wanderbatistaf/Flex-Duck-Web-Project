def27675b665653114acdc7f80bda08b424e1248
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-07-22 21:10:37 -0300

    +ModalVendas(Clientes,Vendor,Produtos)

--- FlexDuck-Web_angular_js/src/app/modals/cliente-modal/cliente-modal.component.ts
+++ FlexDuck-Web_angular_js/src/app/modals/cliente-modal/cliente-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbActiveModal, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import { Clients } from "@app/_models";
+import {map} from "rxjs/operators";
+import {ClientsService} from "@app/_services";
 
 @Component({
   selector: 'app-cliente-modal',
@@ -9,19 +11,14 @@ import { Clients } from "@app/_models";
 })
 export class ClienteModalComponent implements OnInit {
   private modalRef?: NgbModalRef;
-  clientes: Clients[] = [
-    { business_name: 'Cliente 1', cnpj_cpf: '111.111.111-11', telephone: 1111111111 },
-    { business_name: 'Cliente 2', cnpj_cpf: '222.222.222-22', telephone: 2222222222 },
-    { business_name: 'Cliente 3', cnpj_cpf: '333.333.333-33', telephone: 3333333333 },
-    { business_name: 'Cliente 4', cnpj_cpf: '444.444.444-44', telephone: 4444444444 },
-    { business_name: 'Cliente 5', cnpj_cpf: '555.555.555-55', telephone: 5555555555 }
-  ];
+  clientes: Clients[] = [];
 
   constructor(private modalService: NgbModal,
-              public activeModal: NgbActiveModal) { }
+              public activeModal: NgbActiveModal,
+              private clientsService: ClientsService) { }
 
   ngOnInit() {
-    backdrop:true
+    this.getClients()
   }
 
   closeModal() {
@@ -36,19 +33,25 @@ export class ClienteModalComponent implements OnInit {
     }
   }
 
-  adicionarCliente() {
-    // Aqui você pode obter os valores dos campos Nome, CPF e Telefone e adicionar o cliente na lista de clientes.
-    // Exemplo:
-    const nome = (<HTMLInputElement>document.getElementById('nomeCliente')).value;
-    const cpf = (<HTMLInputElement>document.getElementById('cpfCliente')).value;
-    const telefone = (<HTMLInputElement>document.getElementById('telefoneCliente')).value;
-
-    // Adicionar o cliente na lista de clientes (você pode ter uma lista de clientes no seu componente)
-    // Exemplo:
-    // this.listaDeClientes.push({ nome, cpf, telefone });
-
-    // Fechar o modal após adicionar o cliente
-    // this.modalService.dismiss('clienteModal');
+  getClients() {
+    // Recupera todos os pagamentos do servidor
+    this.clientsService.getAll()
+      .pipe(
+        map((response: any) => response.items as Clients[])
+      )
+      .subscribe(
+        // Quando a resposta for bem-sucedida
+        (clientes: Clients[]) => {
+          // Define os pagamentos recuperados na propriedade da classe
+          this.clientes = clientes;
+          // Renderiza os pagamentos
+          console.log(clientes);
+        },
+        // Quando ocorrer um erro na resposta
+        error => {
+          console.log('Houve um erro ao requisitar os clientes.');
+        }
+      );
   }
 
 
