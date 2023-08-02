41518430a849a228a1d3e418e87d80f9bd2c6ebf
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-08-01 23:54:28 -0300

    +API_EnvUpdate

--- FlexDuck_Web_python_api/Controller/mysql_connector.py
+++ FlexDuck_Web_python_api/Controller/mysql_connector.py
@@ -3,15 +3,21 @@ from mysql.connector import pooling
 
 # Configuração do pool de conexões do MySQL
 db_config = {
-    "host": "db4free.net",
+    "host": "db-flexduck.cncrrju5nmty.sa-east-1.rds.amazonaws.com",
     "port": "3306",
-    "user": "flexduck",
+    "user": "duckadmin",
     "password": "lavemopato",
     "database": "flexduckdb",
+
+    # "host": "db4free.net",
+    # "port": "3306",
+    # "user": "flexduck",
+    # "password": "lavemopato",
+    # "database": "flexduckdb",
 }
 
 # Criação do pool de conexões
-db_pool = pooling.MySQLConnectionPool(pool_name="fleduckdb_pool", pool_size=20, **db_config)
+db_pool = pooling.MySQLConnectionPool(pool_name="fleduckdb_pool", pool_size=5, **db_config)
 
 # Função para obter uma conexão do pool
 def get_db_connection():
