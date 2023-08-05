357b9dc13cc2349c43c8f19296f3b2c82667b0e5
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-08-05 16:12:25 -0300

    Update environment.ts
    +UpdateEnv(AWS)

--- FlexDuck-Web_angular_js/src/environments/environment.ts
+++ FlexDuck-Web_angular_js/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     // apiUrl: 'https://ykthsd-5000.csb.app',
     // apiUrl: 'http://127.0.0.1:5000',
-    apiUrl: 'http://15.229.79.164:5000',
+    apiUrl: 'https://15.229.79.164:5000',
     webUrl: 'https://flex-duck-web-project.vercel.app',
     // WebTestUrl: 'http://127.0.0.1:4200'
 };
