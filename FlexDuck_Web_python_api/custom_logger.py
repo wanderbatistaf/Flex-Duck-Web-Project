c9e167c1486c3372735bbfc468aeba2c97d4ac4c
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-08-05 14:38:02 -0300

    +ChangeUWSGItoGuiCorn(log)

--- FlexDuck_Web_python_api/custom_logger.py
+++ FlexDuck_Web_python_api/custom_logger.py
@@ -1,33 +1,26 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
-from concurrent.futures import ThreadPoolExecutor
-
-import logging
-from logging.handlers import RotatingFileHandler
-import sys
-
-
-class CustomLogFormatter(logging.Formatter):
-    def format(self, record):
-        return f"[{record.asctime}|pid: {record.process}|app: {record.app_name}|req: {record.request_id}/{record.application}|{record.remote_addr}] {record.message}"
-
+from flask import request
 
 def setup_logger(app):
     handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=1)
-    formatter = CustomLogFormatter()
+    formatter = logging.Formatter('[%(asctime)s|pid: %(process)d|%(remote_addr)s] %(message)s')
     handler.setFormatter(formatter)
 
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
+    def log_request(environ=None, start_response=None):
+        remote_addr = request.environ.get("REMOTE_ADDR", "-")
+        request_id = request.environ.get("FLASK_REQUEST_ID", "-")
 
+        extra = {'remote_addr': remote_addr}
+        if request_id != "-":
+            extra['request_id'] = request_id
+
+        app.logger.info(
+            f"[pid: {os.getpid()}|{remote_addr}] Request: {request.method} {request.url} from {remote_addr} at {request.host}",
+            extra=extra
+        )
+
+    app.before_request(log_request)
     app.logger.addHandler(handler)
-    app.logger.addHandler(console_handler)  # Adicionar o manipulador para a saída padrão (console)
-
-    app.logger.setLevel(logging.DEBUG)
-
-
-def async_log(record):
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(logging.getLogger().handle, record)
-        future.result()
-
+    app.logger.setLevel(logging.INFO)
