0c80b423818c69552384c365f5eb2209cc5bd305
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-08-18 13:40:25 -0300

    Update environment.prod.ts
    Https-to-Http

--- FlexDuck-Web_angular_js/src/environments/environment.prod.ts
+++ FlexDuck-Web_angular_js/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://54.232.70.133:5000',
+    apiUrl: 'http://54.232.70.133:5000',
     webUrl: 'https://flex-duck-web-project.vercel.app'
 };
