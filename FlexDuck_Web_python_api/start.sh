ee0ad96ecd08a27fab3557587fecc356129b8556
EC2 Default User <ec2-user@ip-172-31-34-228.sa-east-1.compute.internal>
2023-08-21 13:46:50 +0000

    Atualizações no Dockerfile, docker-compose e configurações do Nginx-SSL

--- FlexDuck_Web_python_api/start.sh
+++ FlexDuck_Web_python_api/start.sh
@@ -4,10 +4,10 @@
 service nginx start
 
 # Iniciar o Gunicorn (substitua os detalhes do comando conforme necessário)
-gunicorn main:app --bind 0.0.0.0:5000 --workers 2 --threads 20 --log-level debug &
+gunicorn main:app --bind 0.0.0.0:5000 --workers 2 --threads 20 --certfile ./certified_ssl/fullchain.pem --keyfile ./certified_ssl/privkey.pem --log-level info &
 
-# Obter o certificado Let's Encrypt
-certbot --nginx --non-interactive --agree-tos -m flexduckdev@gmail.com -d flexduckapi.servehttp.com
+# Obter o certificado Let's Encrypt usando o método --webroot
+certbot certonly --webroot -w /var/www/html -d flexapiduck.flexduck.com.br --non-interactive --agree-tos -m flexduckdev@gmail.com
 
 # Manter o contêiner em execução
 tail -f /dev/null
