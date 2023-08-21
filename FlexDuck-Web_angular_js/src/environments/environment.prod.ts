3a1aed0721bf2a6e39501f2d21f6e5aa9d9c2024
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-08-21 15:52:56 -0300

    Update environment.prod.ts
    AWS - FlexDuckApi

--- FlexDuck-Web_angular_js/src/environments/environment.prod.ts
+++ FlexDuck-Web_angular_js/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://54.232.70.133:5000',
+    apiUrl: 'https://flexapiduck.flexduck.com.br',
     webUrl: 'https://flex-duck-web-project.vercel.app'
 };
