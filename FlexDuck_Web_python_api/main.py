a836d315a3af2ab76cae37da8317429ee6470760
Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>
2023-10-02 15:20:35 -0300

    +Notify-FIX
    Signed-off-by: Wanderson Freitas Batista <98125036+wanderbatistaf@users.noreply.github.com>

--- FlexDuck_Web_python_api/main.py
+++ FlexDuck_Web_python_api/main.py
@@ -4,6 +4,7 @@ import subprocess
 import threading
 import time
 from datetime import timedelta
+from threading import Thread
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
@@ -14,6 +15,7 @@ from watchdog.events import FileSystemEventHandler
 from custom_logger import setup_logger
 from Controller.mysql_utils import AppContext
 from Controller.db_connection import get_db_connection
+from Controller.mysql_utils import create_db_pool
 
 # Cria a instância do contexto da aplicação
 app_context = AppContext()
